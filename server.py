192db0af2b4aa8fafb19a47dc232403ce4732944
khouryibrahim <kkibrahim@dons.usfca.edu>
2018-04-05 08:57:54 -0700

    server and html templates

---
+++ server.py
@@ -0,0 +1,26 @@
+import sys
+from flask import Flask, render_template
+from numpy import median
+import ast
+
+word = 'khoury' # THIS WILL COME FROM THE USER SEARCH
+
+app = Flask(__name__)
+
+@app.route("/")
+def search():
+
+    return render_template('search.html')
+
+
+@app.route("/<word>")
+def playlist(word):   
+    
+    # Display the playlist for a word/phrase
+    with open("wordplay/data/sample_output.txt", "r") as f:
+        playlist = f.read()
+        playlist = ast.literal_eval(playlist)
+
+    return render_template('playlist.html', playlist=playlist, word=word)
+
+app.run(host='0.0.0.0', port=80)
