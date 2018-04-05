9b09319391b06629d2cf92e361e1092543df1d90
khouryibrahim <kkibrahim@dons.usfca.edu>
2018-04-05 09:16:18 -0700

    paths updated

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import sys
 from flask import Flask, render_template
-from numpy import median
 import ast
 
 word = 'khoury' # THIS WILL COME FROM THE USER SEARCH
@@ -17,7 +16,7 @@ def search():
 def playlist(word):   
     
     # Display the playlist for a word/phrase
-    with open("wordplay/data/sample_output.txt", "r") as f:
+    with open("data/sample_output.txt", "r") as f:
         playlist = f.read()
         playlist = ast.literal_eval(playlist)
 
