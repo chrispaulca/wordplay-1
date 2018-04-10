e0f6b2c5fff33b468ae082374e68e42abc090e18
Devin Bowers <djbowers@usfca.edu>
2018-04-10 15:36:20 -0700

    made pep8 and added docstrings

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 import sys
-from flask import Flask, render_template, request
 import ast
+from flask import Flask, render_template, request
 from forms import SearchForm
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    """
+    Represents the landing page with a search bar.  Returns a playlist if
+    the user submits a post request
+    """
     search = SearchForm(request.form)
     if request.method == 'POST':
         return playlist(search)
@@ -16,12 +21,16 @@ def index():
 
 @app.route("/results")
 def playlist(search):
-
+    """
+    Reads in the playlist information and displays to the user
+    """
     # Display the playlist for a word/phrase
     with open("data/sample_output.txt", "r") as f:
         playlist = f.read()
         playlist = ast.literal_eval(playlist)
 
-    return render_template('playlist.html', playlist=playlist, word=search.data['search'])
+    return render_template('playlist.html', playlist=playlist,
+                           word=search.data['search'])
+
 
 app.run(host='0.0.0.0', port=80)
