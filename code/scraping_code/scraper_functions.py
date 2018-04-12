5f14e53cac250a6444698de71e8c807577489a7d
chrispaulca <cpcsiszar@usfca.edu>
2018-04-12 13:55:35 -0700

    tweak

--- code/scraping_code/scraper_functions.py
+++ code/scraping_code/scraper_functions.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-"""This module's docstring summary line.
-This is a multi-line docstring. Paragraphs are separated with blank lines.
-Lines conform to 79-column limit.
-Module and packages names should be short, lower_case_with_underscores.
-Notice that this in not PEP8-cheatsheet.py
-Seriously, use flake8. Atom.io with https://atom.io/packages/linter-flake8
-is awesome!
-See http://www.python.org/dev/peps/pep-0008/ for more PEP-8 details
+
+"""These functions scrape lyrics from genius.com given song and artist
+input.
 """
 
 
