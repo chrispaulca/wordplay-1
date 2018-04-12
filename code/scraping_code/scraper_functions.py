54e3d1fc6b4c51cb77471d87a5e7e5cda5866c2d
chrispaulca <cpcsiszar@usfca.edu>
2018-04-12 13:36:11 -0700

    tweak

--- code/scraping_code/scraper_functions.py
+++ code/scraping_code/scraper_functions.py
@@ -38,18 +38,19 @@ def cutt(s, n):
 
 def genius_scrape(document):
     """Takes song info and attempts to grab its lyrics from genius.com.
-    
-    Takes a csv containing song title and artist information and attempts to 
-    scrape each record's 
-    lyrics if available through a generic search on Genius's website. Truncates
-     artist field to two words.
-    Assumes columns named "Song" and "Artist" exist, and that the input is of 
-    csv format. As a 
-    safety measure, splits lengthy arrays into arrays approx 500 records long, 
-    to save progress 
+
+    Takes a csv containing song title and artist information and attempts to
+    scrape each record's
+    lyrics if available through a generic search on Genius's website.
+    Truncates
+    artist field to two words.
+    Assumes columns named "Song" and "Artist" exist, and that the input is of
+    csv format. As a
+    safety measure, splits lengthy arrays into arrays approx 500 records long,
+    to save progress
     periodically and reload the scraping browser between sections.
     """
-    
+
     data_raw = pd.read_csv(document)
 
     artists = data_raw["Artist"].copy()
@@ -59,14 +60,14 @@ def genius_scrape(document):
         artists[i] = cutt(artists[i], 2)
 
     data_raw['artist_trunc'] = artists
-    
+
     # define the search term to input on Genius' webpage
     data_raw["search_term"] = data_raw["Song"].map(str) + ' ' + \
         data_raw["artist_trunc"].map(str)
 
     # split lengthy files into pieces
     if len(data_raw) > 600:
-        collection = np.array_split(data_raw, len(data_raw)%400 + 1)
+        collection = np.array_split(data_raw, len(data_raw) % 400 + 1)
     else:
         collection = []
         collection.append(data_raw)
@@ -76,17 +77,16 @@ def genius_scrape(document):
     # setting options for the browser
     options = webdriver.ChromeOptions()
     prefs = {
-        "profile.managed_default_content_settings.images":2,
-        "profile.managed_default_content_settings.javascript":1
+        "profile.managed_default_content_settings.images": 2,
+        "profile.managed_default_content_settings.javascript": 1
     }
-    options.add_experimental_option("prefs",prefs)
-    
-    
+    options.add_experimental_option("prefs", prefs)
+
     for elem in collection:
-        
+
         iteration += 1
         lyrics_raw = []
-        
+
         # start the browser
         driver = webdriver.Chrome(Path2ChromeDriverEXE, chrome_options=options)
         driver.get('https://genius.com/Ed-sheeran-shape-of-you-lyrics')
@@ -96,52 +96,70 @@ def genius_scrape(document):
             lyrics_gotten = 0
 
             try:
-                #wait only the amount of time necessary for the specified HTML 
+                # wait only the amount of time necessary for the specified HTML
                 # element to appear
                 element = WebDriverWait(driver, 8).until(
-                    EC.presence_of_element_located((By.XPATH, \
-                        "/html/body/div[1]/search-form/form/input"))
+                    EC.presence_of_element_located((By.XPATH,
+                                                    '/ html/body/div[1]/sear'
+                                                    'ch-form/form/input'))
                 )
 
-                search = driver.find_element_by_xpath(\
+                search = driver.find_element_by_xpath(
                     '/html/body/div[1]/search-form/form/input')
-                
+
                 # send search term to search box and submit
                 search.send_keys(i)
                 search.submit()
 
                 element = WebDriverWait(driver, 8).until(
-                        EC.presence_of_element_located((By.XPATH, \
-                            '/html/body/routable-page/ng-outlet/search-'
-                            'results-page/div/div[2]/div[1]/div[2]/search'
-                            '-result-section/div/div[2]/search-result-items/'
-                            'div[1]/search-result-item/div/mini-song-card/a/'
-                            'div[2]'))
-                    )
-                
+                    EC.presence_of_element_located((By.XPATH,
+                                                    '/html/body/routable-pa'
+                                                    'ge/ng-outlet/search-'
+                                                    'results-page/div/div[2'
+                                                    ']/div[1]/div[2]/search'
+                                                    '-result-section/div/di'
+                                                    'v[2]/search-result-items/'
+                                                    'div[1]/search-result-it'
+                                                    'em/div/mini-song-card/a/'
+                                                    'div[2]'))
+                )
+
                 # click the top recommended song link
                 toplink = driver.find_element_by_xpath('/html/body/routable-'
-                    'page/ng-outlet/search-results-page/div/div[2]/div[1]/div'
-                    '[2]/search-result-section/div/div[2]/search-result-items/'
-                    'div[1]/search-result-item/div/mini-song-card/a/div[2]')
+                                                       'page/ng-outlet/searc'
+                                                       'h-results-page/div/di'
+                                                       'v[2]/div[1]/div'
+                                                       '[2]/search-result-sec'
+                                                       'tion/div/div[2]/searc'
+                                                       'h-result-items/'
+                                                       'div[1]/search-result-'
+                                                       'item/div/mini-song-ca'
+                                                       'rd/a/div[2]')
                 toplink.click()
 
                 element = WebDriverWait(driver, 8).until(
-                        EC.presence_of_element_located((By.XPATH, '/html/body/'
-                            'routable-page/ng-outlet/song-page/div/div/div[2]/'
-                            'div[1]/div/defer-compile[1]/lyrics/div/section'))
-                    )
+                    EC.presence_of_element_located((By.XPATH, '/html/body/'
+                                                    'routable-page/ng-outlet'
+                                                    '/song-page/div/div/div[2'
+                                                    ']/'
+                                                    'div[1]/div/defer-compile'
+                                                    '[1]/lyrics/div/section'))
+                )
 
                 # grab lyrics
-                lyrics = driver.find_element_by_xpath('/html/body/routable-page'
-                    '/ng-outlet/song-page/div/div/div[2]/div[1]/div/defer-comp'
-                    'ile[1]/lyrics/div/section')
+                lyrics = driver.find_element_by_xpath('/html/body/routable-p'
+                                                      'age'
+                                                      '/ng-outlet/song-page'
+                                                      '/div/div/div[2]/div['
+                                                      '1]/div/defer-comp'
+                                                      'ile[1]/lyrics/div'
+                                                      '/section')
                 lyrics_text = lyrics.text
 
                 # append lyrics to results
-                lyrics_raw.append(lyrics_text)   
+                lyrics_raw.append(lyrics_text)
                 lyrics_gotten = 1
-        
+
             # not the best except clause but will do for now
             except:
 
@@ -153,21 +171,20 @@ def genius_scrape(document):
         # place scraped lyrics into our dataframe for identification
         elem['lyrics_raw'] = lyrics_raw
 
-        elem.to_csv(document[:-4] + "_lyrics_iteration_%d_marker.csv" % \
-            iteration, encoding='utf-8')
+        elem.to_csv(document[:-4] + "_lyrics_iteration_%d_marker.csv" %
+                    iteration, encoding='utf-8')
 
-        # we quit and restart the browser every so often to clear the browser's 
+        # we quit and restart the browser every so often to clear the browser's
         cache and prevent hangups,
         # timeouts, and hopefully the throttling of suspicious activity
         driver.quit()
 
     # append all sections if necessary
     if len(collection) > 1:
-		interesting_files = glob.glob("*.csv")
-		df_list = []
-		for filename in sorted(interesting_files):
-		    df_list.append(pd.read_csv(filename))
-		full_df = pd.concat(df_list)
+        interesting_files = glob.glob("*.csv")
+        df_list = []
+        for filename in sorted(interesting_files):
+            df_list.append(pd.read_csv(filename))
+        full_df = pd.concat(df_list)
 
-		full_df.to_csv('output.csv')
-
+        full_df.to_csv('output.csv')
