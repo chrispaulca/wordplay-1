afe4c3c99c4b168e9bb96b28cfe056ec4b6f835c
Devin Bowers <djbowers@usfca.edu>
2018-04-12 12:30:03 -0700

    Added environment to hub

--- code/forms.py
+++
@@ -1,6 +0,0 @@
-# forms.py
-
-from wtforms import Form, StringField
-
-class SearchForm(Form):
-    search = StringField('')
--- code/scraping_code/scrape_lyrics.py
+++
@@ -1,4 +0,0 @@
-from scraper_functions import *
-
-for files in ['songs1.csv', 'songs2.csv', 'songs3.csv']:
-    genius_scrape(files)
